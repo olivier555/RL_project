4129f7bc76ca2f56644af2d53f3fe3a646ee1e9a
Pierre Boyeau <pierre.boyeau@gmail.com>
2018-12-07 16:22:48 +0100

    feat(app): coded utility for Kuhn

--- crm.py
+++ crm.py
@@ -3,9 +3,20 @@ from copy import deepcopy
 
 from info_set import hash_dict
 
+
 class History:
-    def __init__(self, terminal_nodes):
-        self.terminal_nodes = terminal_nodes
+    def __init__(self):
+        """
+        Implements history object for Kuhn Poker.
+        - 2 first nodes of traj are the respective cards of players 1 and 2
+        - other nodes, in {0, 1} correspond to respectively pass and bet
+
+        :param terminal_nodes:
+        """
+
+        self.terminal_nodes = [
+            [0, 0], [0, 1, 0], [0, 1, 1], [1, 0], [1, 1]
+        ]
         self.active_player = 0
         self.traj =[]
 
@@ -14,7 +25,52 @@ class History:
         Needs to be adapted to considered problem
         :return: Bool
         """
-        pass
+        if len(self.traj) <= 3:
+            # This means that no more than 1 player has played, game will continue
+            return False
+        else:
+            play_seqs = self.get_play_sequences()
+            return play_seqs in self.terminal_nodes
+
+    def utility(self, player):
+        """
+        Returns utility of selected player assuming we are in a terminal node.
+        :param player: player for which we want utility
+        :return:
+        """
+        assert self.is_terminal()
+        play_seqs = np.array(self.get_play_sequences())
+        player_wins = 2.0*(self.traj[player] > self.traj[1-player]) - 1.0  # valued in -1, 1
+        if (play_seqs == [0, 0]).all():
+            util = 1.0*player_wins
+        elif (play_seqs == [0, 1, 0]).all():
+            util = 2.0*(player == 1) - 1
+        elif (play_seqs == [0, 1, 1]).all():
+            util = 2.0*player_wins
+        elif (play_seqs == [1, 0]).all():
+            util = 2.0*(player == 0) - 1
+        elif (play_seqs == [1, 1]).all():
+            util = 2.0*player_wins
+        else:
+            raise ValueError('Trajectory does not appear to be terminated: {}'.format(play_seqs))
+
+        return util
+
+    def update(self, action):
+        self.traj += [action]
+        self.active_player = 1 - self.active_player
+
+    def get_information_set(self):
+        card_info = self.traj[self.active_player]
+        information_set = {
+            'card_info': card_info,
+            'active_player': self.active_player,
+            'trajectory': self.traj[2:]
+        }
+        return information_set
+
+    def get_play_sequences(self):
+        return self.traj[2:]
 
     def is_chance(self):
         return len(self.traj) < 2
@@ -31,23 +87,11 @@ class History:
         else:
             raise PermissionError
 
-    def update(self, action):
-        self.traj += [action]
-        self.active_player = 1 - self.active_player
 
-    def get_information_set(self):
-        card_info = self.traj[self.active_player]
-        information_set = {
-            'card_info': card_info,
-            'active_player': self.active_player,
-            'trajectory': self.traj[2:]
-        }
-        return information_set
-
-
-def utility(history, player, player1card, player2card):
-    #TODO: implement for Kuhn Poker
-    return
+# def utility(history, player, player1card, player2card):
+#
+#     #TODO: implement for Kuhn Poker
+#     return
 
 
 # We consider below more information sets that appear in reality
@@ -75,12 +119,10 @@ def strategy_update(regrets):
         return 1.0/n_actions*np.ones(n_actions)
 
 
-def crm(history, player, pis):
+def crm(history: History, player, pis):
     pi1, pi2 = pis
     if history.is_terminal():
-        player1card = history.traj[0]
-        player2card = history.traj[1]
-        return utility(history, player, player1card, player2card)
+        return history.utility(player)
     if history.is_chance():
         history.chance_sample()
         return crm(history, player, [pi1, pi2])
